515b0ca534e17368d3ac95ba50b0fe19fa1c92b0
ym <403666612@qq.com>
2017-09-14 20:08:43 +0800

    初步配置django

--- yd_api/settings.py
+++ yd_api/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'yd_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yd_api',
+        'USER': 'root',
+        'PASSWORD': '19931213',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
